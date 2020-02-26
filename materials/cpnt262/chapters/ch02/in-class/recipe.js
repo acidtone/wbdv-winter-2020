b8f87d2178ee82fe8e38eba1bc90e06df9072c76
Tony Grimes <acidtone@tonygrimes.com>
2020-02-26 10:35:49 -0700

    added late-morning in-class

---
+++ materials/cpnt262/chapters/ch02/in-class/recipe.js
@@ -0,0 +1,58 @@
+
+const recipe = {}; 
+
+recipe.title = 'Aioli';
+recipe.serving = '3/4 cup'
+
+recipe.ingredients = [
+  {
+    item: 'clove garlic, finely grated',
+    amount: 1
+  },
+  {
+    item: 'large egg yolks',
+    amount: 2
+  },
+  {
+    item: 'lemon',
+    amount: '2 tsp'
+  },
+  {
+    item: 'salt',
+    amount: '1/4 tsp'
+  },
+  {
+    item: 'sugar',
+    amount: '1/8 tsp'
+  },
+  {
+    item: 'ground white pepper'
+  },
+  {
+    item: 'olive oil',
+    amount: '3/4 cup'
+  }
+];
+
+recipe.directions = 'In large bowl: combine garlic, egg yolks, lemon juice, salt and sugar. Add pepper to taste. Whisking constantly, very slowly drizzle oil into egg mixture until thick and creamy. Season with a dash of something and serve.';
+
+recipe.source = 'https://www.cooksillustrated.com/recipes/7079-spanish-style-toasted-pasta-with-shrimp';
+
+// console.log(recipe);
+
+// Assign a method that returns a custom title based on an argument
+recipe.customTitle = function(something) {
+  // This code only runs when it's invoke with this.customTitle(something)
+  // The `this` keyword of a method points to the object on which the method is attached
+  console.log(this);
+
+  // Now we can access the internal properties and methods without worrying about what the object name is (in this case: recipe)
+  return `${this.title}, with a dash of ${something}!`;
+}
+
+// const customTitle = `${recipe.title}, ${dashOf('pizza')}!`
+console.log(recipe.customTitle('durian'));
+
+// Activity 1: customIngredients()
+
+// Activity 2: customDirections()
