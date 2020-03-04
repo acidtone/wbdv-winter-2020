b552ad08463e23f22aa3c2d1a6b3d63ebf60f079
Tony Grimes <acidtone@tonygrimes.com>
2020-03-04 14:30:26 -0700

    added test commands for mongo data

--- materials/cpnt262/chapters/ch05/activities/mongodb-atlas/starter/app.js
+++ materials/cpnt262/chapters/ch05/activities/mongodb-atlas/starter/app.js
@@ -6,11 +6,20 @@ require('dotenv').config()
 
 // replace the uri string with your connection string.
 const uri = process.env.DB_CONNECTION
+const dbName = 'test';
+const collectionName = 'devices';
 MongoClient.connect(uri,{ useUnifiedTopology: true,useNewUrlParser: true }, function(err, client) {
    if(err) {
         console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
    }
-
+   const collection = client.db(dbName).collection(collectionName);
+   collection.find().forEach(function(doc) {
+      // handle
+      console.log(doc);
+    }, function(err) {
+      // done or error
+      console.log(err);
+    });
    console.log('Connected...');
-   client.close();
+   // client.close();
 });
