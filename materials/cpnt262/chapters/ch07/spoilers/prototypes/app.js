2d0f3014230128fb56c16b4e8653ddab17d76197
Tony Grimes <acidtone@tonygrimes.com>
2020-03-06 08:31:57 -0700

    Added Chapter 7 spoilers

---
+++ materials/cpnt262/chapters/ch07/spoilers/prototypes/app.js
@@ -0,0 +1,31 @@
+const person = {
+    firstname: 'Default',
+    lastname: 'Default',
+    getFullName: function() {
+        return this.firstname + ' ' + this.lastname;  
+    }
+}
+
+const john = {
+    firstname: 'John',
+    lastname: 'Doe'
+}
+
+// don't do this EVER! for demo purposes only!!!
+john.__proto__ = person;
+console.log(john.getFullName());
+console.log(john.firstname);
+
+const jane = {
+    firstname: 'Jane'   
+}
+
+jane.__proto__ = person;
+console.log(jane.getFullName());
+
+person.getFormalFullName = function() {
+    return this.lastname + ', ' + this.firstname;   
+}
+
+console.log(john.getFormalFullName());
+console.log(jane.getFormalFullName());
