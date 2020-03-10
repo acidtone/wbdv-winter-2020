a94a265532f36d2a189a3016f2e7177df38e0794
Tony Grimes <acidtone@tonygrimes.com>
2020-03-10 07:57:47 -0600

    Added Chapter 9

--- materials/cpnt262/chapters/ch09/README.md
+++ materials/cpnt262/chapters/ch09/README.md
@@ -9,3 +9,39 @@
   - Video: [How to Use Express Router](https://www.youtube.com/watch?v=paNikhYqdz0) by Hungry Turtle Code
 - Documentation:
   - [Express Routing](https://expressjs.com/en/guide/routing.html)
+
+## Tool Time: Postman
+- Install Postman
+  - See: [Download the App](https://www.postman.com/downloads/)
+- Postman overview
+  - Request builder
+    - Params
+      - Example: 
+        - [https://jsonplaceholder.typicode.com/posts?userId=1](https://jsonplaceholder.typicode.com/posts?userId=1)
+      - bulk edit
+    - Authorization
+    - Headers
+    - Body
+  - History and Collections
+  - Response window
+    - Body
+      - Pretty
+      - Raw
+      - Preview
+
+## Tool Time: JSON Placeholder
+- [JSONPlaceholder Home Page](https://jsonplaceholder.typicode.com/)
+- [JSONPlaceholder Guide](https://jsonplaceholder.typicode.com/guide.html)
+- [JSONPlaceholder with Postman](https://www.guru99.com/postman-tutorial.html)
+
+## Live Code and Activities
+See: [Starter Code](starter)
+### GET /users
+
+### GET /users/:id
+
+### POST /users
+
+### PUT /users/:id
+
+### DELETE /users/:id
---
+++ materials/cpnt262/chapters/ch09/starter/api-json/app.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+const slugify = require('slugify')
+const Definition = require('./models/Definitions.js');
+
+
+/*************/
+/* App Setup */
+/*************/
+const app = express();
+app.set('view engine','ejs');
+
+/**************************/
+/* Setup Early Middleware */
+/**************************/
+
+// Serve static before any other work is done
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Process JSON data for POST /definitions
+
+app.use(express.json());
+
+/*******************************/
+/* Mongoose/MongoDB Connection */
+/*******************************/
+
+// Set up a pending connection to the database
+// See: https://mongoosejs.com/docs/
+const dbURI = process.env.MONGODB_URL;
+mongoose.connect(dbURI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+
+// Connect to database. Mongoose handles the asynchronous aspects internally so we don't have to.
+var db = mongoose.connection;
+
+// Set a callback in case there's an error.
+db.on('error', function(error){
+  console.log(`Connection Error: ${error.message}`)
+});
+// Set a callback to let us know we're successfully connected
+db.once('open', function() {
+  console.log('Connected to DB...');
+});
+
+/*************************/
+/* /definitions handlers */
+/*************************/
+
+// GET /api/v1/definitions
+
+// GET /api/v1/definitions/example-term
+
+// POST /api/v1/definitions
+
+// PUT /api/v1/definitions/:id
+
+// DELETE /api/v1/definitions/:id
+
+/**************/
+/* Catch 404s */
+/**************/
+
+app.use(function(req, res, next) {
+  res.status(404);
+  res.send('404: File Not Found');
+});
+
+/****************/
+/* Start Server */
+/****************/
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function(){
+  console.log(`Listening on port ${PORT}`);
+});
