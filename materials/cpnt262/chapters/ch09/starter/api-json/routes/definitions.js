6b7209f040cdff77a6ae6669e33b6a5101086926
Tony Grimes <acidtone@tonygrimes.com>
2020-04-23 17:41:43 -0600

    end of session cleanup

---
+++ materials/cpnt262/chapters/ch09/starter/api-json/routes/definitions.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const route = express.Router();
+// Import model
+const Definition = require('../models/Definitions.js');
+
+// GET /api/v1/definitions
+
+// GET /api/v1/definitions/example-term
+
+// POST /api/v1/definitions
+
+// PUT /api/v1/definitions/:id
+
+// DELETE /api/v1/definitions/:id
